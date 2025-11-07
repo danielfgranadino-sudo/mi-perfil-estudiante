019b807a716ca94804c36c423f94af3ca3f5519a
danielfgranadino-sudo <danielfgranadino@gmail.com>
2025-11-07 15:14:15 -0400

    Add Lighthouse CI configuration file

---
+++ lighthouse-config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  ci: {
+    collect: {
+      staticDistDir: './',
+      url: ['http://localhost:8080/perfil.html']
+    },
+    assert: {
+      assertions: {
+        'categories:performance': ['warn', {minScore: 0.8}],
+        'categories:accessibility': ['error', {minScore: 0.9}],
+        'categories:best-practices': ['warn', {minScore: 0.8}],
+        'categories:seo': ['warn', {minScore: 0.8}]
+      }
+    },
+    upload: {
+      target: 'temporary-public-storage',
+    }
+  }
+};
